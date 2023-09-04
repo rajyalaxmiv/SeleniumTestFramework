013dd1a315dff2d440d0435cdeb15732667e8ceb
rajyalaxmiv <144008771+rajyalaxmiv@users.noreply.github.com>
2023-09-04 14:57:08 +0530

    Update conftest.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 @pytest.fixture(scope='class')
 def driver():
     driver = webdriver.Chrome()
-    driver.get(r"https:\\www.Cleartrip.com")
+    driver.get(r"https:\\www.abcxyz.com")
     driver.maximize_window()
     driver.set_page_load_timeout(3)
     yield driver
